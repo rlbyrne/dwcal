2baead78ec624e4e73ea9a10fca251dd2e51c9d0
Ruby Byrne <rlbyrne@uw.edu>
2022-03-31 16:42:26 -0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="DWCal",
+    author="Ruby Byrne",
+    url="https://github.com/rlbyrne/dwcal",
+    scripts=["dwcal/delay_weighted_cal.py", "dwcal/dwcal_tests.py"],
+)
