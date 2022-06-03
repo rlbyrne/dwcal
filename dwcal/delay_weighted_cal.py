correct weight matrix normalization

dwcal/delay_weighted_cal.py
@@ -226,8 +226,8 @@ def cost_function_dw_cal(
 ):
 
     if lambda_val is None:
-        #lambda_val = float(Nbls)
-        lambda_val = 1.
+        # lambda_val = float(Nbls)
+        lambda_val = 1.0
 
     gains = np.reshape(x, (2, Nants, Nfreqs))
     gains = gains[0, :, :] + 1.0j * gains[1, :, :]
@@ -263,8 +263,8 @@ def jac_dw_cal(
 ):
 
     if lambda_val is None:
-        #lambda_val = float(Nbls)
-        lambda_val = 1.
+        # lambda_val = float(Nbls)
+        lambda_val = 1.0
 
     gains = np.reshape(x, (2, Nants, Nfreqs))
     gains = gains[0, :, :] + 1.0j * gains[1, :, :]
@@ -347,8 +347,8 @@ def hess_dw_cal(
 ):
 
     if lambda_val is None:
-        #lambda_val = float(Nbls)
-        lambda_val = 1.
+        # lambda_val = float(Nbls)
+        lambda_val = 1.0
 
     gains = np.reshape(x, (2, Nants, Nfreqs))
     gains = gains[0, :, :] + 1.0j * gains[1, :, :]
@@ -553,7 +553,7 @@ def get_weighted_weight_mat(
             ]
 
     # Make normalization match identity matrix weight mat
-    normalization_factor = Nfreqs * Nbls / np.sum(np.abs(weight_mat))
+    normalization_factor = Nfreqs * Nbls / np.sum(weight_mat)
     weight_mat *= normalization_factor
 
     return weight_mat
@@ -1051,4 +1051,3 @@ def calibrate(
         sys.stdout = stdout_orig
         sys.stderr = stderr_orig
         log_file_new.close()
-        